efd9bf146924229a40e892d29eec93c53ad5410e
Matt M. Dedek <matthew.dedek@gmail.com>
2014-08-10 09:46:02 -0700

    Updated plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,16 @@
-source("load_dataset.R")
+load.epc <- function(){
+  fp <- "data/household_power_consumption.txt"
+  tmp <- read.table(fp, header = TRUE, nrows = 5, sep=";")
+  classes <- sapply(tmp, class)
+  
+  
+  df <- read.table(fp, sep=";", header=TRUE, na.strings="?", colClasses = classes, nrows = 2080000)
+  df <- df[df$Date == "1/2/2007" | df$Date == "2/2/2007",]
+  df$TimeStamp <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+  df$Date <- as.Date(df$Date, format="%d/%m/%Y")
+  return(df)
+}
 
-# Load the clean, raw data
 df <- load.epc()
 
 png("plot4.png", width=480, height=480)
@@ -23,3 +33,4 @@ plot(df$TimeStamp, df$Global_reactive_power, type="n", ylab="Global_reactive_pow
 lines(df$TimeStamp, df$Global_reactive_power, lwd=1, col="black")
 
 dev.off()
+
